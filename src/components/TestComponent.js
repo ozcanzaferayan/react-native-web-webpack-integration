58acf729863fe16548f8df8a27015569d3862857
Zafer AYAN <ozcanzaferayan@gmail.com>
2022-07-20 16:30:37 +0300

    Test change action

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -4,7 +4,7 @@ import {View, Text} from 'react-native';
 function TestComponent() {
   return (
     <View>
-      <Text>Hello from React Native</Text>
+      <Text>Hello from React Native 2</Text>
     </View>
   );
 }
